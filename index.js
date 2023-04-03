8de1d92194b5cd5c1b3150cbf1b766899664adc0
Khairul Haaziq <rhycoz@yahoo.com>
2023-04-03 17:40:42 +0800

    add

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import qrcode from "qrcode-terminal";
 import pkg from "whatsapp-web.js";
-const { Client, LocalAuth } = pkg;
+const { Client, RemoteAuth } = pkg;
 import { ConversationChain } from "langchain/chains";
 import { ChatOpenAI } from "langchain/chat_models";
 import {
@@ -14,6 +14,8 @@ import dotenv from "dotenv";
 import yaml from "js-yaml";
 import fs from "fs";
 dotenv.config();
+import { MongoStore } from "wwebjs-mongo";
+import mongoose from "mongoose";
 
 const config = yaml.load(fs.readFileSync("config.yaml", "utf-8"));
 
@@ -32,51 +34,80 @@ let chain = new ConversationChain({
 	llm: chat,
 });
 
-let client = new Client({ authStrategy: new LocalAuth() });
-client.on("qr", (qr) => {
-	qrcode.generate(qr, { small: true });
-});
-client.on("ready", () => {
-	console.log("Client is ready!");
-});
-client.initialize();
+// Load the session data
+mongoose.connect(process.env.MONGODB_URI).then(async () => {
+	const store = new MongoStore({ mongoose: mongoose });
+	const client = new Client({
+		authStrategy: new RemoteAuth({
+			store: store,
+			backupSyncIntervalMs: 300000,
+		}),
+	});
 
-let lastMessageTime = 0;
-let historyEmpty = true;
+	const sessionExists = await store.sessionExists({
+		session: "yourSessionName",
+	});
 
-client.on("message", async (message) => {
-	const currentTime = new Date().getTime();
-	const elapsedTime = (currentTime - lastMessageTime) / (1000 * 60);
-	if (message.body === "!chat delete history" || (elapsedTime >= 30 && !historyEmpty) ) {
-		chain = new ConversationChain({
-			memory: new BufferMemory({
-				returnMessages: true,
-				memoryKey: "history",
-			}),
-			prompt: chatPrompt,
-			llm: chat,
+	if (sessionExists) {
+		console.log("session exists");
+	} else {
+		client.on("qr", (qr) => {
+			qrcode.generate(qr, { small: true });
 		});
-		historyEmpty = true;
-		if (message.body === "!chat delete history") {
-			message.reply("history deleted");
-		}
-		return;
 	}
-	const numberOfWords = message.body.split(" ").length;
-	if (
-		message.body.startsWith(`${config.trigger} `) &&
-		numberOfWords > 1
-	) {
-		const contents = message.body.split(" ").slice(1).join(" ");
-		chain.call({ input: contents })
-			.then((res) => {
-				message.reply(res.response);
-			})
-			.catch((error) => {
-				console.log(error);
-				message.reply("error");
+	client.on("ready", () => {
+		console.log("Client is ready!");
+	});
+	client.on("remote_session_saved", async () => {
+		console.log("saved session");
+	});
+
+	client.initialize();
+
+	let lastMessageTime = 0;
+	let historyEmpty = true;
+
+	client.on("message", async (message) => {
+		const currentTime = new Date().getTime();
+		const elapsedTime =
+			(currentTime - lastMessageTime) / (1000 * 60);
+		if (
+			message.body === "!chat delete history" ||
+			(elapsedTime >= 30 && !historyEmpty)
+		) {
+			chain = new ConversationChain({
+				memory: new BufferMemory({
+					returnMessages: true,
+					memoryKey: "history",
+				}),
+				prompt: chatPrompt,
+				llm: chat,
 			});
-		lastMessageTime = currentTime;
-		historyEmpty = false;
-	}
+			historyEmpty = true;
+			if (message.body === "!chat delete history") {
+				message.reply("history deleted");
+			}
+			return;
+		}
+		const numberOfWords = message.body.split(" ").length;
+		if (
+			message.body.startsWith(`${config.trigger} `) &&
+			numberOfWords > 1
+		) {
+			const contents = message.body
+				.split(" ")
+				.slice(1)
+				.join(" ");
+			chain.call({ input: contents })
+				.then((res) => {
+					message.reply(res.response);
+				})
+				.catch((error) => {
+					console.log(error);
+					message.reply("error");
+				});
+			lastMessageTime = currentTime;
+			historyEmpty = false;
+		}
+	});
 });
